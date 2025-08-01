9e37a958f6d45697762b878f4155df89dd5f117e
Markis <markusdep005@gmail.com>
2025-08-01 15:12:18 +0200

    colorato interfaccia

---
+++ tutorialgit/src/tutgit/interfacciagit2.java
@@ -0,0 +1,50 @@
+package tutgit;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+
+public class interfacciagit2 extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					interfacciagit2 frame = new interfacciagit2();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public interfacciagit2() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(128, 128, 255));
+		panel.setBounds(84, 62, 172, 128);
+		contentPane.add(panel);
+		panel.setLayout(null);
+	}
+
+}
